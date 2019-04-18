d3ce5cd2c0b6cff9e0e255d8b0a553c4f027510e
Aglae Verduzco <aglae.cv@gmail.com>
2019-04-18 02:08:34 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,12 +3,13 @@ require_relative './class_methods_module.rb'
 
 class Kid
   extend MetaDancing
-end
-  include Dance
 
   attr_accessor :name
 
   def initialize(name)
     @name = name
   end
+end
+
+  include Dance
 end
